68c37bc7737e50f31a57a75323d201d247e1398c
TalariAnushka <155547836+TalariAnushka@users.noreply.github.com>
2025-03-29 09:20:10 +0000

    conversionOperstions

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,23 @@
+// let score = "33abc"
+// console.log(typeof(score))
+// let valueInNumber =Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+// //nan - not a number
+//when u  initilaize  nul after conversion to a number it will show 0 in the ouput
+// and null type is object
+//when you define undefined in the intilization it will give u  nan in the outafter after conversion to a number
+// true->1 , false ->0
+//  "33"=>33
+//"33abc" =>nan
+//  let isLoggedIn=1
+//  let booleanIsLoggedIn =Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+// output is true
+// when you define string ="" => false
+//when you define string ="anu"=>true
+// 1=>true ; 0=>false
+let someNumber =33
+let stringNumber =String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
