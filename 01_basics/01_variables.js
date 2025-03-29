87883b4daba526c474acf9dcb41438842b71624b
TalariAnushka <155547836+TalariAnushka@users.noreply.github.com>
2025-03-29 08:53:14 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId=144553
+let accountEmail= "anushkatalari08@gmail.com";
+var accountPassword ="12345"
+accountCity ="Jaipur"
+let  accountState;
+// in output it gives undefined in the output 
+//let is a  scope 
+/* 
+ prefer not to use var
+ because of issues in block scope  and functional scope
+*/
+//accountId=2
+console.log(accountId);
+console.table([accountId, accountEmail,accountPassword,accountCity])
