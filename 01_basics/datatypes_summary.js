94d7bc30c43b094bb45042403016a1fa70bde4b5
TalariAnushka <155547836+TalariAnushka@users.noreply.github.com>
2025-03-29 10:50:58 +0000

    datatypessummary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,42 @@
+//primitive datatype
+
+//7 types : String ,Number , Boolean , null=>(empty), undefined, symbol,
+// BigInt
+
+
+// const score =100
+// const scoreValue  =100.3
+//  const isLoggedIn =false
+//  const outsideTemp=null
+//  let userEmail;
+//  const id =Symbol('123')
+//  const anotherId =Symbol('123')
+// console.log(id===anotherId)
+const bigNumber = 345668899999532478n;
+
+
+// reference (non-primitive)
+//Arrays, Objects, Functions
+
+
+//arrays
+const heros =["shaktiman","naagraj","doga"]
+//objects
+ let myObj ={
+    name :"hitesh",
+    age:22,  
+}
+//functions
+const myFunction =  function(){
+    console.log("Hello World");
+}
+console.log(myFunction)
+console.log(typeof(bigNumber))
+
+// type of operator results
+// undefined = undefined
+// null = object
+//  boolean = boolean
+// number = number
+// string = string
+ //object = object
