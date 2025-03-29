493ced995fc9d6f8e6c424e69c1e13c69de08cd2
TalariAnushka <155547836+TalariAnushka@users.noreply.github.com>
2025-03-29 14:06:39 +0000

    datatypes_summary

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -44,16 +44,32 @@ console.log(typeof(bigNumber))
 
 
 
-Exploring JavaScript's Basic Data Types:
+//Exploring JavaScript's Basic Data Types:
 
-JavaScript has seven primitive data types: String, Number, Boolean, null, undefined, symbol, and BigInt. Here's a quick snapshot to help you understand each one:
+// JavaScript has seven primitive data types: String, Number, Boolean, null, undefined, symbol, and BigInt. Here's a quick snapshot to help you understand each one:
 
-- `String`: Used for text, for example: `"Hello, World!"`.
-- `Number`: For numeric values, e.g., `100` or `100.3`.
-- `Boolean`: Represents truth values, `true` or `false`.
-- `null`: To represent an intentional absence of any object value, i.e., `null` (empty).
-- `undefined`: Represents a variable that has been declared but not assigned a value.
-- `symbol`: A unique and immutable data type, often used to identify object properties, such as `Symbol('123')`.
-- `BigInt`: For large integers.
+// - `String`: Used for text, for example: `"Hello, World!"`.
+// - `Number`: For numeric values, e.g., `100` or `100.3`.
+// - `Boolean`: Represents truth values, `true` or `false`.
+// - `null`: To represent an intentional absence of any object value, i.e., `null` (empty).
+// - `undefined`: Represents a variable that has been declared but not assigned a value.
+// - `symbol`: A unique and immutable data type, often used to identify object properties, such as `Symbol('123')`.
+// - `BigInt`: For large integers.
 
-Here's some sample code demonstrating these types:
+// stack, heap
+ // stack - primitive type- you get copy
+ //heap - non primitive- reference of  the original
+ //let myYoutubename = "anushka";
+// let  anothername =myYoutubename
+// anothername = "chaiaurcode"
+// console.log(myYoutubename)
+// console.log(anothername)
+
+let userOne ={
+    email :"user@google.com",
+     upi :"user@ybl"
+}
+ let userTwo =userOne
+  userTwo.email ="anushka@gmail.com"
+  console.log(userOne.email)
+  console.log(userTwo.email)
