e0c92d7aa75396a2419ebedd746792d61a64c504
TalariAnushka <155547836+TalariAnushka@users.noreply.github.com>
2025-03-29 08:52:22 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict"; // treat all js code as newer version
+//alert("hello") we  are using nodejs, not browser
+
+//number => 2 to power 53
+// bigint
+//string => " "
+//boolean =>true/false
+//null => standalone value -> empty
+//undefined => value iis not assigned
+//symbol =>  unique 
+
+
+//object 
+
+console.log(typeof 18)
