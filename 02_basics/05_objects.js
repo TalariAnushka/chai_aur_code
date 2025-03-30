a5739d66175e593d76d0ec4c92f503ad48399f0a
TalariAnushka <155547836+TalariAnushka@users.noreply.github.com>
2025-03-30 05:08:49 +0000

    objects

--- 02_basics/05_objects.js
+++ 02_basics/05_objects.js
@@ -1,32 +1,32 @@
-const course ={
-    coursename :"js in hindi",
-    price :"999",
-    courseInstructor :"anushka"
-}
-// course.courseInstructor
-const{courseInstructor:instructor}=course
+// const course ={
+//     coursename :"js in hindi",
+//     price :"999",
+//     courseInstructor :"anushka"
+// }
+// // course.courseInstructor
+// const{courseInstructor:instructor}=course
 
-// console.log(courseInstructor); // output is also anushkaa
- console.log(instructor) // output is also anushka
+// // console.log(courseInstructor); // output is also anushkaa
+//  console.log(instructor) // output is also anushka
 
-// //destructor
-//  const navbar =({comapany}) =>{
+// // //destructor
+// //  const navbar =({comapany}) =>{
 
-//  }
-//  nav(company ="anushka")
-
-
-//JSON IS OBJECT =>{}
-    // {
-    //     "name" :"hitesh",
-    //     "coursename" :"js in hindi",
-    //     "price" :"999"
-    // }
+// //  }
+// //  nav(company ="anushka")
 
 
-    //api's
-[
-{},
-{},
-{},
-    ]
+// //JSON IS OBJECT =>{}
+//     // {
+//     //     "name" :"hitesh",
+//     //     "coursename" :"js in hindi",
+//     //     "price" :"999"
+//     // }
+
+
+//     //api's
+// [
+// {},
+// {},
+// {},
+//     ]
