01a9e7f40562e33ca34ad49ac4cab6d530584be6
TalariAnushka <155547836+TalariAnushka@users.noreply.github.com>
2025-03-30 04:46:07 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,49 @@
+//arrays
+// storing a  collection of multiple items  under a single variable
+// js array is  resizabel
+// in js arrays can be mix of mutiple datatypes
+// 0 based indexing
+
+//shallow copy - a shallow copy of an object is a  copy whose property share the same references
+
+//deep  copy - don't  copy the same reference
+
+const myArr = [0,1,2,3,4,5]
+// const myHeros =["shaktiman", "naagraj"]
+// const myarr2 = new Array(1,2,3,4)
+// console.log(myArr[0]);
+
+//Array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+// console.log(myArr)
+
+// myArr.unshift(9)// insertion is done at the starting
+// myArr.shift()// deletion is done at the starting
+// console.log(myArr);
+// console.log(myArr.includes(9));//  if the array ahve the given value it gives true  if not it gives false
+// console.log(myArr.indexOf(19)); // if the value is present it gives index og the given value   if it is not present then it  gives -1  in the output
+// const newArr = myArr.join()  // adds all the elemnets  of an array into a string seperated  by the specified seperator
+// console.log(myArr)
+// console.log(newArr)
+
+//slice ,splice
+
+// console.log("A",myArr); //  the array is named as A
+
+// const myn1 = myArr.slice(1,3)// not includes index 3 // return the section of an array
+// console.log(myn1);
+// console.log("B", myArr); // the array is named as B
+// const  myn2 = myArr.splice(1,3)// here the last value is included in the output array
+// console.log(myn2);
+
+
+
+
+
+
+
+
+
