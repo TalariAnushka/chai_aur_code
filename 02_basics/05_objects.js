011b3c9b7acc3d4c298bc847eeb575d3ca78c9a6
TalariAnushka <155547836+TalariAnushka@users.noreply.github.com>
2025-03-30 05:08:07 +0000

    objects

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,32 @@
+const course ={
+    coursename :"js in hindi",
+    price :"999",
+    courseInstructor :"anushka"
+}
+// course.courseInstructor
+const{courseInstructor:instructor}=course
+
+// console.log(courseInstructor); // output is also anushkaa
+ console.log(instructor) // output is also anushka
+
+// //destructor
+//  const navbar =({comapany}) =>{
+
+//  }
+//  nav(company ="anushka")
+
+
+//JSON IS OBJECT =>{}
+    // {
+    //     "name" :"hitesh",
+    //     "coursename" :"js in hindi",
+    //     "price" :"999"
+    // }
+
+
+    //api's
+[
+{},
+{},
+{},
+    ]
